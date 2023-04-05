17ef9a8c21787f09a112dcd5a700965b5fb3f70d
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-04-05 21:21:22 +0300

    added currency drop sheet for currency formating

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -11,6 +11,27 @@ class _settingspageState extends State<settingspage> {
   // this is added for the drawer later on
   int _selectedIndex = 2;
 
+  // list of currenvies
+  String _selectedCurrency = "\$ (USD)";
+  List<String> _currencies = [
+    '\$ (USD) - US Dollar',
+    '€ (EUR) - Euro',
+    '£ (GBP) - British Pound',
+    '¥ (JPY) - Japanese Yen',
+    '\$ (CAD) - Canadian Dollar',
+    '\$ (AUD) - Australian Dollar',
+    'Fr (CHF) - Swiss Franc',
+    '¥ (CNY) - Chinese Yuan Renminbi',
+    'лв (BGN) - Bulgarian Lev',
+    'R\$ (BRL) - Brazilian Real',
+    'Kč (CZK) - Czech Koruna',
+    'kr (DKK) - Danish Krone',
+    '\$ (HKD) - Hong Kong Dollar',
+    'kn (HRK) - Croatian Kuna',
+    'Ft (HUF) - Hungarian Forint',
+    '₹ (INR) - Indian Rupee',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,13 +106,63 @@ class _settingspageState extends State<settingspage> {
                             child: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
-                                  Text('USD', style: TextStyle(fontSize: 15,color: Colors.grey)), // Placeholder for selected value
+                                  Text('${_selectedCurrency}', style: TextStyle(fontSize: 15,color: Colors.grey)), // Placeholder for selected value
                                   SizedBox(width: 10,),
                                   Icon(Icons.arrow_forward_ios,size: 20,),
                                 ]),
                           ),
+
+                          // here when tapped on arrow currency dropsheet pops up
                           onTap: () {
                             // TODO: Navigate to the currency selection page.
+                            showModalBottomSheet(
+                              context: context,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(20),
+                                ),
+                              ),
+                              builder: (BuildContext context) {
+                                return Container(
+                                  height: 400,
+                                  padding: EdgeInsets.symmetric(vertical: 16),
+                                  child: ListView.builder(
+                                    itemCount: _currencies.length,
+                                    itemBuilder: (BuildContext context, int index) {
+                                      final currencyParts = _currencies[index].split(' - ');
+                                      final currencyCode = currencyParts[0];
+                                      final currencyName = currencyParts[1];
+                                      return Padding(
+                                        padding: EdgeInsets.symmetric(horizontal: 16),
+                                        child: ListTile(
+                                          title: Text(
+                                            currencyCode,
+                                            style: TextStyle(
+                                              fontSize: 22,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            currencyName,
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                          trailing: _selectedCurrency == currencyCode
+                                              ? Icon(Icons.check, color: Colors.green)
+                                              : null,
+                                          onTap: () {
+                                            setState(() {
+                                              _selectedCurrency = currencyCode;
+                                            });
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                            );
                           },
                         ),
 
@@ -115,9 +186,9 @@ class _settingspageState extends State<settingspage> {
                           ),
                           onTap: () {
                             // TODO: Navigate to the currency selection page.
+
                           },
                         ),
-
                       ],
                     ),
                   )
