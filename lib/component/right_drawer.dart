394613da606344be15df7c3d375c61b56c697d7d
Home <moathdarwish2020@gmail.com>
2023-04-08 19:01:45 +0300

    Updated Alert Dialog functionality

--- lib/component/right_drawer.dart
+++ lib/component/right_drawer.dart
@@ -85,6 +85,97 @@ class RightDrawer extends StatelessWidget {
                   ListTile(
                     leading: Icon(Icons.logout),
                     onTap: () {
+                      onItemTapped(3);
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            elevation: 250,
+                            backgroundColor: Colors.white,
+                            title: Text(
+                              'Sign Out',
+                              style: TextStyle(
+                                color: Color.fromRGBO(255, 107, 53, 1),
+                                fontFamily: "K2D",
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            content: Text(
+                              'Are you sure you want to sign out?',
+                              style: TextStyle(
+                                color: Color(0XFF145756),
+                                fontFamily: "K2D",
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            actions: [
+                              Container(
+                                width: 80,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(255, 107, 53, 1),
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(30),
+                                      bottomLeft: Radius.circular(30)),
+                                ),
+                                padding: EdgeInsets.all(10),
+                                child: TextButton(
+                                  child: Text(
+                                    'Yes',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "K2D",
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    FirebaseController().signOutUser(context);
+                                    Navigator.of(context).pushAndRemoveUntil(
+                                      MaterialPageRoute(
+                                          builder: (context) => loginPage()),
+                                      (route) => false,
+                                    );
+                                  },
+                                ),
+                              ),
+                              Container(
+                                width: 80,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(50),
+                                      bottomLeft: Radius.circular(50)),
+                                ),
+                                padding: EdgeInsets.all(10),
+                                child: TextButton(
+                                  child: Text(
+                                    'Cancel',
+                                    style: TextStyle(
+                                      color: Color(0XFF145756),
+                                      fontFamily: "K2D",
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+
+                  /*ListTile(
+                    leading: Icon(Icons.logout),
+                    onTap: () {
                      onItemTapped(3);
                       showDialog(
                         context: context,
@@ -142,7 +233,7 @@ class RightDrawer extends StatelessWidget {
                         },
                       );
                     },
-                  ),
+                  ),*/
                 ],
               ),
             ),
