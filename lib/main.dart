29529956fb52370f80984dbf1baea950e269836f
Abdallah Taqiedin <72511000+abdullah-taqiaddin@users.noreply.github.com>
2023-04-17 02:09:19 +0300

    Revert "auto login implemented"
    This reverts commit afafd571026bf5f77b8a529f414b1c5e5a3e840c.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:testapp/view/budget_page.dart';
 import 'package:testapp/view/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:testapp/viewmodel/auth_provider.dart';
 
-late Widget home;
-
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  // Check if the user is already authenticated
-  User? user = FirebaseAuth.instance.currentUser;
-  home = user == null ? loginPage() : BudgetPage();
-
-  // Check if saved user credentials are available and use them for auto-login
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? userEmail = prefs.getString('userEmail');
-  String? userPassword = prefs.getString('userPassword');
-
-  if (userEmail != null && userPassword != null) {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: userEmail, password: userPassword);
-      home = BudgetPage();
-    } catch (e) {
-      // Ignore errors and fall back to the default home widget
-    }
-  }
   runApp(
     ChangeNotifierProvider(
       create: (_) => AuthProvider(),
@@ -66,6 +43,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return home;
+    return loginPage();
   }
 }
--- lib/viewmodel/firebase_controller.dart
+++ lib/viewmodel/firebase_controller.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testapp/viewmodel/auth_provider.dart';
 import 'package:testapp/model/user_model.dart';
 
@@ -44,83 +43,41 @@ class FirebaseController{
   bool isTokenExpired() {
     return false;
   }
-  //
-  // Future<UserCredential> signInWithGoogle(BuildContext context) async {
-  //
-  //     // Trigger the authentication flow
-  //     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-  //
-  //     // Obtain the auth details from the request
-  //     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-  //
-  //     // Create a new credential
-  //     final credential = GoogleAuthProvider.credential(
-  //       accessToken: googleAuth?.accessToken,
-  //       idToken: googleAuth?.idToken,
-  //     );
-  //
-  //
-  //     try {
-  //       // Once signed in, return the UserCredential
-  //       final userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-  //
-  //       Provider.of<AuthProvider>(context, listen: false).setUserCredentials(userCredential);
-  //       return userCredential;
-  //
-  //     } catch (e) {
-  //       // Sign the user out and try again
-  //       await GoogleSignIn().signOut();
-  //       return signInWithGoogle(context);
-  //     }
-  //
-  // }
 
   Future<UserCredential> signInWithGoogle(BuildContext context) async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-    try {
-      //نسيف لل sharedpreferences
-      final userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-      await saveUserCredentials(userCredential);
-      Provider.of<AuthProvider>(context, listen: false).setUserCredentials(userCredential);
-      print(userCredential);
-      return userCredential;
-    } catch (e) {
-      print(e);
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
-      await GoogleSignIn().signOut();
-      //try and sign in again?
-      //TODO:research a better way
-      return signInWithGoogle(context);
-    }
-  }
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
 
-  Future<void> saveUserCredentials(UserCredential userCredential) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    await prefs.setString('userEmail', userCredential.user!.email!);
-    await prefs.setString('userPassword', userCredential.credential!.token!.toString());
-    print(prefs.toString());
+      try {
+        // Once signed in, return the UserCredential
+        final userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+        Provider.of<AuthProvider>(context, listen: false).setUserCredentials(userCredential);
+        return userCredential;
+
+      } catch (e) {
+        // Sign the user out and try again
+        await GoogleSignIn().signOut();
+        return signInWithGoogle(context);
+      }
 
   }
 
-  signOutUser(BuildContext context) async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
+  signOutUser(BuildContext context) {
     try {
-      //we need to clear the local storage
-      pref.clear();
       Provider.of<AuthProvider>(context, listen: false).removeUserCredentials();
       firebaseAuth.signOut();
-
     }
     catch(e){
       //TODO:Make a snackbar if any errors occure
