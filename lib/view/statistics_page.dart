608ed155e2c58787b2e32c56e93e0e9ae204fbca
Ali_Labadi <ali.mohammed.labadi@gmail.com>
2023-05-19 21:15:36 +0300

    added expense box plus some styling

--- lib/view/statistics_page.dart
+++ lib/view/statistics_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'budget_page.dart';
+import 'expense_page.dart';
 
 class StatisticsPage extends StatefulWidget {
   const StatisticsPage({Key? key}) : super(key: key);
@@ -22,16 +23,85 @@ class _StatisticsPageState extends State<StatisticsPage> {
             color: Colors.white,
             image: DecorationImage(image: AssetImage("assets/images/background-cropped.jpg"),fit: BoxFit.fill,opacity: 0.5)
         ),
-        child: ListView.builder(
-          itemCount: cardData.length,
-          itemBuilder: (context, index) {
-            final card = cardData[index];
-            return ListTile(
-              title: Text(card['type'] ?? ''),
-              subtitle: Text(card['spent'] ?? ''),
-            );
-          },
-        ),
+
+
+
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 100.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Top 5 budgets',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height / 3.5,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        itemCount: cardData.length,
+                        itemBuilder: (context, index) {
+                          final card = cardData[index];
+                          return ListTile(
+                            title: Text(card['type'] ?? ''),
+                            subtitle: Text(card['total'] ?? ''),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50.0), // Add space of 50.0 between the boxes
+              Container(
+                child: Column(
+                  children: [
+                    Text(
+                      'Top 5 expenses',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height / 3.5,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        itemCount: cardData.length,
+                        itemBuilder: (context, index) {
+                          final anotherCard = cardData[index];
+                          return ListTile(
+                            title: Text(anotherCard['type'] ?? ''),
+                            subtitle: Text(anotherCard['spent'] ?? ''),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+
+
+
+
+
       ),
     );
   }
